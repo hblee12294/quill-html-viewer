3e4159db7dfe652860627d95c5c6b84126a2bb70
hblee12294 <hblee12294@gmail.com>
2022-07-16 01:40:09 -0700

    feat: add prettier

--- src/components/Highlighter.tsx
+++ src/components/Highlighter.tsx
@@ -1,28 +1,35 @@
-import { FC, ReactNode, useEffect, useRef } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import hljs from 'highlight.js'
 import xml from 'highlight.js/lib/languages/xml'
 import 'highlight.js/styles/github.css'
+// import 'highlight.js/styles/monokai.css'
+import pretty from 'pretty'
+
+import styles from './Highlighter.module.css'
 
 hljs.registerLanguage('xml', xml)
 
 interface HighlighterProps {
-  children?: ReactNode
+  children: string
 }
 
 export const Highlighter: FC<HighlighterProps> = ({ children }) => {
   const codeRef = useRef<HTMLElement>(null)
 
+  const [htmlContent, setHTMLContent] = useState(children)
+
   useEffect(() => {
     if (!codeRef.current) return
 
-    hljs.highlightElement(codeRef.current)
+    const beautifiedContent = pretty(children)
+    const highlightedHtml = hljs.highlight(beautifiedContent, { language: 'xml' }).value
+
+    setHTMLContent(highlightedHtml)
   }, [children])
 
   return (
-    <pre>
-      <code ref={codeRef} className="xml">
-        {children}
-      </code>
+    <pre className={styles.pre}>
+      <code ref={codeRef} className="hljs" dangerouslySetInnerHTML={{ __html: htmlContent }}></code>
     </pre>
   )
 }
